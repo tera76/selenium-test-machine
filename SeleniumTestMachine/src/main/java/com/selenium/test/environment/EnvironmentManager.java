3fbdf7d0878b1dd754a5d39238bb50b646e7d686
tera <andrea.matera@drivek.com>
2020-02-05 02:00:10 +0100

    firefox driver

--- SeleniumTestMachine/src/main/java/com/selenium/test/environment/EnvironmentManager.java
+++ SeleniumTestMachine/src/main/java/com/selenium/test/environment/EnvironmentManager.java
@@ -2,16 +2,30 @@ package com.selenium.test.environment;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class EnvironmentManager {
 
-    public static void initWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        WebDriver driver = new ChromeDriver();
-        RunEnvironment.setWebDriver(driver);
-    }
+	public static void initWebDriver() {
+		final String driverType = "FIREFOX";
 
-    public static void shutDownDriver() {
-        RunEnvironment.getWebDriver().quit();
-    }
+		WebDriver driver = null;
+		switch (driverType) {
+			default:
+			case "CHROME":
+				System.setProperty("webdriver.chrome.driver", "chromedriver");
+				driver = new ChromeDriver();
+				break;
+			case "FIREFOX":
+				System.setProperty("webdriver.gecko.driver", "geckodriver");
+				driver = new FirefoxDriver();
+				break;
+		}
+
+		RunEnvironment.setWebDriver(driver);
+	}
+
+	public static void shutDownDriver() {
+		RunEnvironment.getWebDriver().quit();
+	}
 }
